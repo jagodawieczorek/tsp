f258cb5086dd2fb49872e01bf5b5ccbcc8bd3e4d
Jagoda Wieczorek <jagoda.maria.wieczorek@gmail.com>
2020-05-03 17:52:45 +0200

    Travelling salesman problem (TSP) representation. Initial commit.

---
+++ src/main/java/Runner.java
@@ -0,0 +1,12 @@
+import tsp.TSP;
+
+/**
+ * Runner class
+ *
+ * @author Jagoda Wieczorek
+ */
+public class Runner {
+	public static void main(final String[] args) {
+		final TSP tsp = new TSP("resources/tsp/gr96.tsp");
+	}
+}
